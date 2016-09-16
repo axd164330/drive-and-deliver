e2e78786a342422124076608b97efd65d5e6f8ed
axd164330 <axd164330@utdallas.edu>
2016-09-15 19:38:23 -0500

    Changed the context path and added maven files

--- src/com/driveanddeliver/WelcomeController.java
+++ src/com/driveanddeliver/WelcomeController.java
@@ -3,16 +3,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
-
+/**
+ * 
+ * @author asdhammu
+ *
+ */
 @Controller
-@RequestMapping("/hello")
-public class HelloController{
+@RequestMapping("/")
+public class WelcomeController{
  
    @RequestMapping(method = RequestMethod.GET)
    public String printHello(ModelMap model) {
-      model.addAttribute("message", "Hello Spring MVC Framework!");
+      model.addAttribute("message", "Welcome to Drive and Deliver");
 
-      return "hello";
+      return "index";
    }
 
 }
+
+
