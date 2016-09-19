eb306336ffff8c4aa03c052ea059081202677f1d
axd164330 <axd164330@utdallas.edu>
2016-09-18 20:59:31 -0500

    Added the hibernate connections with the web application

--- src/com/driveanddeliver/controller/HomepageController.java
+++ src/com/driveanddeliver/controller/HomepageController.java
@@ -1,18 +1,34 @@
-package com.driveanddeliver;
+package com.driveanddeliver.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.driveanddeliver.model.User;
+import com.driveanddeliver.service.UserService;
+
 import org.springframework.ui.ModelMap;
 /**
  * 
- * @author asdhammu
+ * @author Amandeep Singh Dhammu
  *
  */
 @Controller
 public class HomepageController{
  
+	
+   private UserService userService;
+      
+   @Autowired(required=true)
+   @Qualifier(value="userService")
+   public void setUserService(UserService userService) {
+	   this.userService = userService;
+   }
+
    @RequestMapping(value="/",method = RequestMethod.GET)
    public String printHello(ModelMap model) {
+	  
       model.addAttribute("message", "Welcome to Drive and Deliver");
 
       return "index";
@@ -27,7 +43,13 @@ public class HomepageController{
    @RequestMapping(value="/accountSummary",method=RequestMethod.GET)
    public String getAccountDetails(ModelMap model){
 	   
-	   
+		User user = new User();
+		user.setName("Amandeep"); 
+		user.setCountry("India");
+		
+		this.userService.save(user);
+		
+		
 	   return "account";
    }
 
---
+++ src/com/driveanddeliver/service/UserService.java
@@ -0,0 +1,9 @@
+package com.driveanddeliver.service;
+
+import com.driveanddeliver.model.User;
+
+public interface UserService {
+
+	public void save(User user);
+	
+}
